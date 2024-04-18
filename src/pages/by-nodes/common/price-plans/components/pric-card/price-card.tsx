4f6c55237fb11357f9b3dbfff15796828fe77353
shinydev618 <shinydev618@gmail.com>
2024-04-18 20:11:43 +0500

    wip: bug fixing

--- src/pages/by-nodes/common/price-plans/components/pric-card/price-card.tsx
+++ src/pages/by-nodes/common/price-plans/components/pric-card/price-card.tsx
@@ -1,33 +1,31 @@
-import { CheckIcon } from "../../../../../../icons";
-import { PriceCard as PriceCardComponent } from "./style";
-import { Props } from "./types";
+import { CheckIcon } from '../../../../../../icons'
+import { PriceCard as PriceCardComponent } from './style'
+import { Props } from './types'
 export const PriceCard = ({
   priceType,
   onClick,
   title,
   buttonTitle,
   items,
+  selected,
 }: Props) => {
   return (
     <PriceCardComponent
-      // style={{
-      //   borderColor: priceType === "full" ? "#FBDA00" : "",
-      //   background: priceType === "full" ? "#1D1D03" : "transparent",
-      // }}
-      data-aos="fade-up"
+      data-aos='zoom-in'
+      className={selected === priceType ? 'active' : ''}
     >
-      <img src="/assets/images/logo.png" alt="" />
+      <img src='/assets/images/logo.png' alt='' />
       <h2>{title}</h2>
       <hr />
       <p>Services included</p>
       <ul>
         {items.map((list, index) => (
           <li key={index}>
-            <CheckIcon style={{ color: "#08F02D" }} /> <span>{list}</span>
+            <CheckIcon style={{ color: '#08F02D' }} /> <span>{list}</span>
           </li>
         ))}
       </ul>
       <button onClick={onClick}>{buttonTitle}</button>
     </PriceCardComponent>
-  );
-};
+  )
+}
