ae57eddba8f2d16368001a94c0dd21e2a5f50ef6
shinydev618 <shinydev618@gmail.com>
2024-05-14 10:44:09 -0400

    updated text

--- src/constants/header.ts
+++ src/constants/header.ts
@@ -3,16 +3,16 @@ export const HEADER_LINKS = [
     path: '/',
     label: 'Home',
   },
-  {
-    path: '/subcription',
-    label: 'Buy zkHive Subcription',
-  },
+  // {
+  //   path: '/subcription',
+  //   label: 'Buy zkHive Subcription',
+  // },
   {
     path: '/buy-node',
-    label: 'Buy Node',
+    label: 'Buy Nodes',
   },
   {
     path: '/my-node',
-    label: 'My Node',
+    label: 'My Nodes',
   },
 ]
